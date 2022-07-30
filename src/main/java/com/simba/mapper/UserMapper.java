4b7330c47924c8d184a789208f2679f5bef5d7f3
LGXfufile <401040990@qq.com>
2022-07-30 19:39:57 +0800

    修复注解异常

--- src/main/java/com/simba/mapper/UserMapper.java
+++ src/main/java/com/simba/mapper/UserMapper.java
@@ -4,12 +4,13 @@ package com.simba.mapper;
 
 import com.simba.pojo.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Mapper//表示是mybatis的mapper类,这是Dao层；
-@ResponseBody//这里也可以用@Component,只是为了分层；
+@Repository // 这里也可以用@Component,只是为了分层；
 public interface UserMapper {
 
     List<User> queryUserAll();
